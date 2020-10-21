90d3a255923e0b993e7567f16cc8b4ddaf3c6f62
George Galanakis <ggalan87@gmail.com>
2020-10-21 03:35:42 +0300

    Stub for affiliate sites

---
+++ src/views/HeraklionDIY.js
@@ -0,0 +1,36 @@
+// @flow
+import React, {Component} from "react";
+import "assets/css/social_bar.css";
+import {Col, Container, Row} from "reactstrap";
+import {get_default_meta} from "../default_meta";
+import DocumentMeta from "react-document-meta";
+import GoogleMapReact from "google-map-react";
+
+import { render } from 'react-dom'
+import L from 'leaflet';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+
+class HeraklionDIY extends Component {
+
+    render() {
+        return (
+            <DocumentMeta {...get_default_meta({title: 'HeraklionDIY'})}>
+                {window.dispatchEvent(new CustomEvent('new_page'))}
+                <div className="main">
+                    <div className="section text-center">
+                        <Container>
+                            <Row>
+                                <Col className="ml-auto mr-auto" md="8">
+                                    <h2 className="title text-left">Heraklion DIY affiliate projects</h2>
+
+                                </Col>
+                            </Row>
+                        </Container>
+                    </div>
+                </div>
+            </DocumentMeta>
+        )
+    }
+}
+
+export default HeraklionDIY;
