98ff89bb9cf38662ad28f9c42032649f967498cd
George Galanakis <ggalan87@gmail.com>
2020-11-09 00:30:29 +0200

    Added static data until api is ready

---
+++ src/data/articles.js
@@ -0,0 +1,47 @@
+export const articles = [
+    {
+        src: require('data/articles/gravanis.md'),
+        title: 'Γραβάνη τα φαρμάκια σου, μ’ αφήσανε φιντάνι',
+        created_at: 'Mon, 19 Oct 2020 12:00:00 GMT',
+        tags: [ 
+            { id: 0, name: 'υγεία'},
+            { id: 1, name: 'πολιτική'}
+            ],
+        short_description: 'Κείμενο σχολιασμού για τις δημόσιες τοποθετήσεις του Α. Γραβάνη για Covid19 κ.α.',
+        id: 0,
+    },
+    {
+        src: require('data/articles/fyla3h.md'),
+        title: 'Φύλαξη πανεπιστημίου (προς Σύγκλητο)',
+        created_at: 'Mon, 10 Feb 2020 12:00:00 GMT',
+        tags: [ 
+            { id: 0, name: 'πανεπιστήμιο'},
+            { id: 1, name: 'φύλαξη'}
+            ],
+        short_description: 'Κείμενο παρεμβασης στη Σύγκλητο για τη φύλαξη του ΠΚ, με αφορμή τα γεγονότα του Δεκέμβρη',
+        id: 1
+    },
+    {
+        src: require('data/articles/makro19.md'),
+        title: 'Μακροβούτες 2019',
+        created_at: 'Mon, 11 Mar 2019 12:00:00 GMT',
+        tags: [ 
+            { id: 0, name: 'μακροβουτες'},
+            { id: 1, name: 'κάλεσμα'}
+            ],
+        short_description: 'Κάλεσμα για τις μακροβούτες του 2019',
+        id: 2
+    },
+    {
+        src: require('data/articles/enisa.md'),
+        title: 'Φοιτητικές εστίες, ENISA και ευρωπαϊκό σχολείο. Μια σειρά από λάθος προτεραιότητες.',
+        created_at: 'Fri, 30 Nov 2018 12:00:00 GMT',
+        tags: [ 
+            { id: 0, name: 'πανεπιστήμιο'},
+            { id: 1, name: 'εστίες'},
+            { id: 2, name: 'enisa'}
+            ],
+        short_description: 'Κείμενο παρεμβασης για τις κτιριακές προτεραιότητες στο ΠΚ',
+        id: 3
+    }
+]
---
+++ src/data/paintsData.js
@@ -0,0 +1,23 @@
+export const photos =
+[
+    {
+        src: require('assets/img/acts/science_cooperation.jpg'),
+        width: 1.33,
+        height: 1
+    },
+    {
+        src: require('assets/img/acts/stop_wars.jpg'),
+        width: 1,
+        height: 1.33
+    },
+    {
+        src: require('assets/img/acts/toilo.jpg'),
+        width: 2,
+        height: 1
+    },
+    {
+        src: require('assets/img/acts/graduation.jpg'),
+        width: 1.22,
+        height: 1
+    }
+]
