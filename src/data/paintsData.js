12cab082bb7eafd41ead9e6969cb2d152a74bdfb
George Galanakis <ggalan87@gmail.com>
2020-11-14 23:00:01 +0200

    added some more paintings

--- src/data/paintsData.js
+++ src/data/paintsData.js
@@ -19,5 +19,30 @@ export const photos =
         src: require('assets/img/acts/graduation.jpg'),
         width: 1.22,
         height: 1
+    },
+    {
+        src: require('assets/img/acts/snake.jpg'),
+        width: 1,
+        height: 2.05
+    },
+    {
+        src: require('assets/img/acts/nina.jpg'),
+        width: 1.28,
+        height: 1
+    },
+    {
+        src: require('assets/img/acts/mushroom.jpg'),
+        width: 1.33,
+        height: 1
+    },
+    {
+        src: require('assets/img/acts/louis.jpg'),
+        width: 1.26,
+        height: 1
+    },
+    {
+        src: require('assets/img/acts/church.jpg'),
+        width: 1.32,
+        height: 1
     }
 ]
